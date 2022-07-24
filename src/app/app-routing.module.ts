e8796f84ba15301fa85a21ce9b73f33e0357c7c5
Fernando Arturo Valerio <fernandoarturo06@gmail.com>
2022-07-24 11:43:11 -0400

    Lobby Component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LobbyComponent } from './lobby/lobby.component';
+
+const routes: Routes = [
+  { path: '', redirectTo:'/lobby', pathMatch: 'full' },
+  {path: 'lobby', component: LobbyComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LobbyComponent } from './lobby/lobby.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LobbyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
